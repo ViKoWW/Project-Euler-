8bf04c9cd5959fb3b329e11b5915840564892bb1
ViKoWW <50905430+ViKoWW@users.noreply.github.com>
2021-09-02 17:42:12 +0200

    Update Problem2-Even_Fibonacci_Numbers.cpp

--- Problem2-Even_Fibonacci_Numbers.cpp
+++ Problem2-Even_Fibonacci_Numbers.cpp
@@ -1,1 +1,35 @@
+#include "pch.h"
+#include <iostream>
+
+int start1 = 0;
+int start2 = 1;
+const int limit = 4000000;
+int evenSum = 0;
+//funktion som räknar ut fibonacci sekvensen samt identifierar om ena parametern, num2, är jämnt, isåfall läggs det till i summan evenSum.
+void evaluateFibonacci(int num1, int num2) {
+	//checkar om talet är jämnt
+	if ((num2 % 2) == 0)
+	{
+		evenSum += num2;
+	//	std::cout << evenSum << std::endl;
+	}
+	//funktionen som tar fram fibonacci sekvensen, görs genom rekursion då funktionen kallar sig själv om och om igen tills det att det större numret är större än 4 miljoner. 
+	int placeholder;
+	placeholder = num1;
+	num1 = num2;
+	num2 = placeholder + num2;
+	//std::cout << num1 << std::endl;
+	if(num2 < limit)
+		evaluateFibonacci(num1, num2);
+	
+}
+
+
+int main()
+{
+	//main funktionen, kör fibonacci funktionen med starttalen 0 och 1. 
+	evaluateFibonacci(start1, start2);
+	//till slut printas den slutgiltiga summan
+	std::cout << "The final sum is: " << evenSum;
+}
 
